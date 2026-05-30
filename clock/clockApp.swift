b9e8fca2d4e368e035a0e6cfa3a1716f11b2192d
배건우 <102368729+mclusky0626@users.noreply.github.com>
2026-05-30 15:59:37 +0900

    Initial Commit

---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by 배건우 on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
